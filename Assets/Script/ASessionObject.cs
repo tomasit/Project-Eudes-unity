025e19d161827fa89c567c0829a65b5883cb447c
tomasit <thomas.itel@epitech.eu>
2023-01-16 20:15:02 +0900

    feat : add sessionObject abstraction + DataStructure utility

---
+++ Assets/Script/ASessionObject.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ASessionObject : MonoBehaviour
+{
+    [SerializeField] protected bool _inPause = false;
+    [SerializeField] protected bool _started = false;
+    [SerializeField] protected bool _demoObject = false;
+    public abstract void StartSession();
+    public abstract void StopSession();
+    public abstract void SaveSessionData();
+    public bool CanRun() { return _inPause && _started; }
+    public bool Pause() { return _started ? _inPause = true : false; }
+    public void Resume() { _inPause = false; }
+}
